ea24021230d981e676b400a40ebd0e241a1a8f25
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-05-25 17:37:34 -0300

    feat: Valor inicial do nome de usuário

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -4,6 +4,8 @@ import { ArrowRight } from 'phosphor-react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const registerFormSchema = z.object({
   username: z
@@ -24,12 +26,21 @@ export default function Register() {
   const {
     register,
     handleSubmit,
+    setValue,
     formState: { errors, isSubmitting },
   } = useForm<RegisterFormData>({
     resolver: zodResolver(registerFormSchema),
   })
 
-  function handleRegister(data: RegisterFormData) {
+  const router = useRouter()
+
+  useEffect(() => {
+    if (router.query.username) {
+      setValue('username', String(router.query.username))
+    }
+  }, [router.query?.username, setValue])
+
+  function handleRegister(data: RegisterFormData): void {
     console.log(data)
   }
 
