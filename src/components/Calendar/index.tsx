2a49580eb781157717979ae43a1cda3b3f1d4fb6
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-05-29 17:34:52 -0300

    feat: Estrutura visual do calendário

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,60 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+import { getWeekDays } from '@/utils/get-week-days'
+
+export function Calendar() {
+  const shortWeekDays = getWeekDays({ short: true })
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Dezembro <span>2022</span>
+        </CalendarTitle>
+
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}.</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th></th>
+            <th>
+              <CalendarDay>1</CalendarDay>
+            </th>
+            <th>
+              <CalendarDay>2</CalendarDay>
+            </th>
+            <th>
+              <CalendarDay>3</CalendarDay>
+            </th>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
