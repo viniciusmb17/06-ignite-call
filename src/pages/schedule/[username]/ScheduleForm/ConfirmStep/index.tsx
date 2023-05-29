df1ff892b6574e2f76b9df7114a0de6dea9e6e7b
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-05-29 18:38:50 -0300

    feat: Página de confirmação

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,3 +1,47 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+import { CalendarBlank, Clock } from 'phosphor-react'
+
 export function ConfirmStep() {
-  return <div>Confirm Step</div>
+  function handleConfirmScheduling(data: any) {
+    console.log(data)
+  }
+  return (
+    <ConfirmForm
+      as="form" /* onSubmit={handleSubmit(handleConfirmScheduling)} */
+    >
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de Setembro de 2022
+        </Text>
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size={'sm'}>Nome Completo</Text>
+        <TextInput placeholder="Seu nome" />
+      </label>
+
+      <label>
+        <Text size={'sm'}>Endereço de e-mail</Text>
+        <TextInput type="email" placeholder="johndoe@example.com" />
+      </label>
+
+      <label>
+        <Text size={'sm'}>Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant={'tertiary'}>
+          Cancelar
+        </Button>
+        <Button type="submit">Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
