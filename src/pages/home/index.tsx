530484620e4cb62548a99db42f414325318c061c
Vinicius M. Barbosa <viniciusmbarbosa@hotmail.com>
2023-06-11 14:13:46 -0300

    feat: Otimização de SEO

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,30 +4,37 @@ import { Container, Hero, Preview } from './styles'
 import previewImage from '../../assets/app-preview.png'
 import Image from 'next/image'
 import { ClaimUsernameForm } from './components/ClaimUsernameForm'
+import { NextSeo } from 'next-seo'
 
 export default function Home() {
   return (
-    <Container>
-      <Hero>
-        <Heading as={'h1'} size={'4xl'}>
-          Agendamento descomplicado
-        </Heading>
-        <Text size="xl">
-          Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo
-        </Text>
-        <ClaimUsernameForm />
-      </Hero>
+    <>
+      <NextSeo
+        title="Descomplique sua agenda | Ignite Call"
+        description="Conecte seu calendário e permita que as pessoas marquem agendamentos no seu tempo livre"
+      />
+      <Container>
+        <Hero>
+          <Heading as={'h1'} size={'4xl'}>
+            Agendamento descomplicado
+          </Heading>
+          <Text size="xl">
+            Conecte seu calendário e permita que as pessoas marquem agendamentos
+            no seu tempo
+          </Text>
+          <ClaimUsernameForm />
+        </Hero>
 
-      <Preview>
-        <Image
-          src={previewImage}
-          height={400}
-          alt="Calendário simbolizando aplicação em funcionamento"
-          quality={100}
-          priority
-        />
-      </Preview>
-    </Container>
+        <Preview>
+          <Image
+            src={previewImage}
+            height={400}
+            alt="Calendário simbolizando aplicação em funcionamento"
+            quality={100}
+            priority
+          />
+        </Preview>
+      </Container>
+    </>
   )
 }
